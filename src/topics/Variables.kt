9a186bd739cf85d1584529d901c7084e7c2e5dff
Egor Anchutin <zbit16@gmail.com>
2021-05-11 13:56:42 +0300

    Lear about variables

---
+++ src/topics/Variables.kt
@@ -0,0 +1,16 @@
+package topics
+
+fun main() {
+    val test11 = "Hello world"
+
+    var test22 = "Hello"
+    test22 = "Goodbye"
+
+    val test1 = "Hello world"
+
+    var test2 = 2
+    test2 = 4
+
+    println("Values of test2 ${test1 + test2}")
+    println("Values of test2 ${2 + 2}")
+}
